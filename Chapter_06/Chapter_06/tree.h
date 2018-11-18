7b84999886ddc118cb3d847a12a8eab29b67fe4f
Sepheranix <sepheranix@gmail.com>
2018-11-18 23:45:26 +0800

    Update tree.h

--- Chapter_06/Chapter_06/tree.h
+++ Chapter_06/Chapter_06/tree.h
@@ -1,8 +1,7 @@
 #ifndef _TREE_H_
 #define _TREE_H_
 
-/* 树的双亲表示法，P135*/
-
+//----- 树的双亲表示法，P135 -----
 #define MAX_TREE_SIZE 100
 typedef struct {	//结点结构
 	TElemType data;
@@ -13,5 +12,34 @@ typedef struct {	//树结构
 	PTNode nodes[MAX_TREE_SIZE];
 	int r, n;	//根的位置和结点数
 }PTree;
+
+
+//----- 树的孩子链表存储表示 ------
+typedef struct CTNode {	//孩子结点
+	int child;
+	struct CTNode *next;
+} *ChiledPtr;
+
+typedef struct {
+	TElemType data;
+	ChiledPtr firstchild;	//孩子链表头指针
+}CTBox;
+
+typedef struct {
+	CTBox nodes[MAX_TREE_SIZE];
+	int n, r;	//结点数和根的位置
+}CTree;
+
+
+//------ 树的二叉链表（孩子-兄弟）存储表示 -------
+typedef struct CSNode {
+	TElemType data;
+	struct CSNode *firstchild, *nextsibling;
+}CSNode, *CSTree;
+
+//void CreatTree(CSTree &T);
+//int TreeDepth(CSTree T);
+//void AllPath(BiTree T, SqStack &S);
+//void OutPath(BiTree T, SqStack &S);
 
 #endif // !_TREE_H_
